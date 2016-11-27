490dbda574c6f7aa723f7a56ed5f71471aaa975e
liujingcheng <cyliujingcheng@126.com>
2016-11-27 12:03:50 +0800

    初步建好项目

---
+++ LjcDesktopApp/ViewModel/AutoPlanViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CanYouLib.ExcelLib;
+using GalaSoft.MvvmLight.Command;
+
+namespace LjcDesktopApp.ViewModel
+{
+    public class AutoPlanViewModel
+    {
+        /// <summary>
+        /// 导入计划Excel
+        /// </summary>
+        public RelayCommand ImportPlanCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+               {
+                   var importExcel = new ImportExcel();
+                   var dialog =
+                       new Microsoft.Win32.OpenFileDialog { Filter = "excel|*.xls" };
+                   if (dialog.ShowDialog() == true)
+                   {
+                       var sourceFileName = dialog.FileName;
+                       var ds = importExcel.ImportDataSet(sourceFileName, false, 1, 1);
+                   }
+               });
+            }
+        }
+
+        /// <summary>
+        /// 导出计划Excel
+        /// </summary>
+        public RelayCommand ExportPlanCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                });
+            }
+        }
+    }
+}
